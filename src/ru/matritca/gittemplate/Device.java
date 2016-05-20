8a3a8083942e497586ae28c25ea9fd6f8188f509
vasiliy <localducks@gmail.com>
2016-05-20 22:48:23 +0300

    Add getter and setter

--- src/ru/matritca/gittemplate/Device.java
+++ src/ru/matritca/gittemplate/Device.java
@@ -10,4 +10,12 @@ public class Device {
     public Device(String deviceName) {
         this.deviceName = deviceName;
     }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
 }
