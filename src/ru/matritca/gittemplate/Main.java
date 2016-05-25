542583b2dc26e78e6877e5e9bf751638842659e7
vasiliy <localducks@gmail.com>
2016-05-25 22:27:03 +0300

    Add getter and setter

--- src/ru/matritca/gittemplate/Main.java
+++ src/ru/matritca/gittemplate/Main.java
@@ -2,7 +2,7 @@ package ru.matritca.gittemplate;
 
 public class Main {
 
-    private String s;
+    private String s2;
 
     public static void main(String[] args) {
         System.out.println("First version");
