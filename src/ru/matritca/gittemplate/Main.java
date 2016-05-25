bdaaa32503df91926432c7702f6e702467dbb2f0
vasiliy <localducks@gmail.com>
2016-05-25 22:38:19 +0300

    Add getter and setter

--- src/ru/matritca/gittemplate/Main.java
+++ src/ru/matritca/gittemplate/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First version");
         System.out.println("Second version");
-        System.out.println("Third");
+         
         System.out.println("Third version");
         System.out.println("Third version");
         System.out.println("Third ");
